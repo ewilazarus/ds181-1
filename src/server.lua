cd6346b366acc6d99635ed9d302445aceb55c82c
Gabriel Lima <ewilazarus@localhost.localdomain>
2018-03-23 20:36:39 -0300

    Creates master socket

--- src/server.lua
+++ src/server.lua
@@ -1,10 +1,22 @@
+local socket = require('socket')
+
 -- Generates a random string of size 1024
-local function generateDownloadContent()
+local function generateDownloadContent(byteSize)
     local content = ''
-    for i = 1, 1024 do
+    for i = 1, byteSize do
         content = content .. string.char(math.random(48, 122))
     end
     return content
 end
 
-return {generateDownloadContent = generateDownloadContent}
+local Server = {}
+Server.MasterSocket = {}
+
+function Server.MasterSocket:spawn()
+    self.socket = assert(socket.bind('*', 0))
+    local ip, port = self.socket:getsockname()
+    self.ip = ip
+    self.port = port
+end
+
+return {generateDownloadContent = generateDownloadContent, Server = Server}
--- tests.lua
+++ tests.lua
@@ -2,8 +2,16 @@ local lu = require('luaunit')
 local server = require('src.server')
 
 function testGenerateDownloadContent()
-    local downloadContent = server.generateDownloadContent()
-    lu.assertEquals(downloadContent:len(), 1024)
+    local size = 1024
+    local downloadContent = server.generateDownloadContent(size)
+    lu.assertEquals(downloadContent:len(), size)
+end
+
+function testSpawnMasterSocket()
+    local Server = server.Server
+    Server.MasterSocket:spawn()
+    lu.assertEquals(Server.MasterSocket.ip, '0.0.0.0')
+    lu.assertTrue(tonumber(Server.MasterSocket.port) > 1024)
 end
 
 print(lu.LuaUnit.run('--name', './tests'))
